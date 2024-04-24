a0f0a3e8d38865d444e064a4a09618472d4f7cfe
Nick Tolley <nic.m.tolley@gmail.com>
2024-04-23 21:04:51 -0400

    WIP

--- code/utils.py
+++ code/utils.py
@@ -9,6 +9,111 @@ from sklearn.model_selection import ShuffleSplit
 import os
 from sklearn.preprocessing import StandardScaler
 
+# LSTM autoencoder modules
+class model_lstm(nn.Module):
+    def __init__(self, input_size, hidden_dim, n_layers, dropout, device='cpu', bidirectional=False):
+        super(model_lstm, self).__init__()
+
+        #multiplier based on bidirectional parameter
+        if bidirectional:
+            num_directions = 2
+        else:
+            num_directions = 1
+
+        # Defining some parameters
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers * num_directions
+        self.device = device
+        self.dropout = dropout
+        self.bidirectional = bidirectional
+        self.input_size = input_size
+
+        self.lstm = nn.LSTM(self.input_size, self.hidden_dim, n_layers, batch_first=True, dropout=dropout, bidirectional=bidirectional) 
+    
+    def forward(self, x):
+        batch_size = x.size(0)
+        # Initializing hidden state for first input using method defined below
+        hidden, cell = self.init_hidden(batch_size)
+
+        out, (hidden, cell) = self.lstm(x, (hidden, cell))
+
+        return out
+    
+    def init_hidden(self, batch_size):
+        weight = next(self.parameters()).data.to(self.device)
+
+        #LSTM initialization
+        hidden = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(self.device)
+        cell = weight.new(self.n_layers, batch_size, self.hidden_dim).zero_().to(self.device) + 1
+
+        return hidden, cell
+
+
+class model_lstm_autoencoder(nn.Module):
+    def __init__(self, input_size, lstm_encoder_hidden, lstm_decoder_hidden,
+                 encoder_layer_size, decoder_layer_size, bottleneck=10, device='cpu'):
+        super(model_lstm_autoencoder, self).__init__()
+        self.input_size = input_size
+        self.encoder_layer_size, self.decoder_layer_size = encoder_layer_size, decoder_layer_size
+        self.lstm_encoder_hidden, self.lstm_decoder_hidden = lstm_encoder_hidden, lstm_decoder_hidden
+        self.bottleneck = bottleneck
+
+        self.encoder_lstm = model_lstm(input_size=input_size, hidden_dim=lstm_encoder_hidden, n_layers=2, dropout=0.1)
+        self.encoder_ann = model_ann(input_size, bottleneck, layer_size=self.encoder_layer_size)
+        self.decoder_ann = model_ann(bottleneck, input_size, layer_size=self.decoder_layer_size)
+        self.decoder_lstm = model_lstm(input_size=input_size, hidden_dim=lstm_encoder_hidden, n_layers=2, dropout=0.1)
+
+        self.fc = nn.Linear(self.input_size, self.input_size)
+
+    def forward(self, x):
+        out = self.encoder_lstm(x)
+        out = out.contiguous()
+        out = self.encoder_ann(out)
+
+        out = self.decoder_ann(out)
+        out = self.decoder_lstm(out)
+        out = self.fc(out)
+        return out
+
+
+# Feed forward network autoencoder modules
+class model_ann(nn.Module):
+    def __init__(self, input_size, output_size, layer_size, dropout=0.0):
+        super(model_ann, self).__init__()
+        self.input_size,  self.layer_size, self.output_size = input_size, layer_size, output_size
+
+        #List layer sizes
+        self.layer_hidden = np.concatenate([[input_size], layer_size, [output_size]])
+        
+        #Compile layers into lists
+        self.layer_list = nn.ModuleList(
+            [nn.Linear(in_features=self.layer_hidden[idx], out_features=self.layer_hidden[idx+1]) for idx in range(len(self.layer_hidden)-1)] )
+
+        self.dropout_list = [nn.Dropout(p=dropout) for _ in range(len(self.layer_list)-1)]
+ 
+    def forward(self, x):
+        #Encoding step
+        for idx in range(len(self.layer_list) - 1):
+            x = torch.tanh(self.layer_list[idx](x))
+            x = self.dropout_list[idx](x)
+        x = self.layer_list[-1](x)
+        return x
+
+class model_ann_autoencoder(nn.Module):
+    def __init__(self, input_size, output_size, encoder_layer_size, decoder_layer_size, bottleneck = 10):
+        super(model_ann_autoencoder, self).__init__()
+        self.input_size, self.output_size = input_size, output_size
+        self.encoder_layer_size, self.decoder_layer_size = encoder_layer_size, decoder_layer_size
+
+        self.encoder = model_ann(input_size, bottleneck, layer_size=self.encoder_layer_size)
+        self.decoder = model_ann(bottleneck, input_size, layer_size=self.decoder_layer_size)
+
+    def forward(self, x):
+        out = self.encoder(x)
+        out = self.decoder(out)
+
+        return out
+
 #Helper function to pytorch train networks for decoding
 def train_validate_model(model, optimizer, criterion, max_epochs, training_generator, validation_generator, device, print_freq=10, early_stop=20):
     train_loss_array = []
